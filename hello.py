56d79b29db99a7f8d9bc60e267943525effe3dc0
SeanAbbott1 <sabbott2012@gmail.com>
2016-01-12 16:45:30 -0500

    Change1

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 # replace the word 'hector' with your own name
 
 def hello_world():
-	return 'hello world, my name is '
+	return 'hello world, my name is Sean Abbott'
 
 print( hello_world() + 'hector' )
